5709a1c1f3bc9b441ac51de411e11cc9763aed7e
anshi <anshuojha299@gmail.com>
2024-01-02 12:01:34 +0530

    ** added

--- operators.js
+++ operators.js
@@ -11,3 +11,6 @@ console.log("%:",a%b)
 console.log("--:",(--a) + (--b)) //--a =39 , --b=29 pre-increment/decrement first update then store 
 console.log("++:",(a++) + (b++)) //a++=39 , b++=29 (after storing current value it increases the value by 1) post-increment/decrement first store then update
 console.log("a:",a,"b:",b)
+
+// ** used find out power
+console.log(5**4)
