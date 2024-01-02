63d755c6490a3c07930954f52dd8b065a459bb56
anshi <anshuojha299@gmail.com>
2024-01-02 12:06:56 +0530

    comparision operators

--- operators.js
+++ operators.js
@@ -14,3 +14,17 @@ console.log("a:",a,"b:",b)
 
 // ** used find out power
 console.log(5**4)
+
+// comparision operators ==, ===, >=,<=,<,>,!==,!= it return boolean value
+
+let num1=10
+let num2=20
+
+console.log(num1==num2) //flase
+console.log(num1===num2) //flase
+console.log(num1>=num2) //flase
+console.log(num1<=num2) //true
+console.log(num1!=num2) //true
+console.log(num1!==num2) //true
+console.log(num1>num2) //false
+console.log(num1<num2) //true
