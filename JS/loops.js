0fe8c81e419c1a164fb62a99ecafbbbad85a8781
anshi <anshuojha299@gmail.com>
2024-01-12 01:21:45 +0530

    all loops

--- JS/loops.js
+++ JS/loops.js
@@ -1,4 +1,19 @@
 // for loop
+var sum=0;
 for(let i=1;i<=10;i++){
     console.log("2 x ",i," =",2*i)
+    sum+=i;
  }
+ console.log("Sum: ",sum);
+
+ let i=0
+ while(i<=10){
+    console.log(i)
+    i++;
+ }
+
+ let j=1
+ do{
+    console.log("3 x ",j," =",3*j)
+    j++
+ }while(j<=10)
