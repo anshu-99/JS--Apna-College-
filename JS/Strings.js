fd0f8278ad7f965af887726e1465ca45393004ba
anshi <anshuojha299@gmail.com>
2024-01-15 16:43:56 +0530

    functions

---
+++ JS/Strings.js
@@ -0,0 +1,25 @@
+let sr="Udbhav"
+let str1='Ojha'
+
+console.log(sr,str1)
+
+// template literals 
+var money=56
+let st=`Hey I have ${money} rupees`
+console.log(st)
+
+console.log("Udbhav \n Ojha")
+let strr=sr.toUpperCase();
+console.log(str)
+let newStr=sr.toLowerCase();
+console.log(newStr);
+let newSr=sr.trim();
+console.log(newSr);
+
+var string="012345678"
+console.log(string.slice(1,8))
+console.log(sr.concat(str1))
+
+let ans=string.replace('23','66')
+console.log(ans)
+console.log(ans.charAt(3))
